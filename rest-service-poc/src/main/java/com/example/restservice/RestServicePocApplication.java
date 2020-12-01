38a174b662d666c9d96ef12b42abc20c02393add
Ramesh Nagaraju <ramesh2014@gmail.com>
2020-12-01 21:52:50 +0530

    new changes

---
+++ rest-service-poc/src/main/java/com/example/restservice/RestServicePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicePocApplication.class, args);
+	}
+
+}
